7a6b811853df4d1987141c1b4726dd92eda98d27
Juan Pablo Addeo <jpaddeo@gmail.com>
2023-05-16 23:36:51 -0300

    refactor: :rocket: adding new methods and improve auth with more info to user creation

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -1,10 +1,14 @@
 import { handleAuth, handleCallback } from '@auth0/nextjs-auth0';
 
-import { createUser } from '../../../libs/db';
+import user from '@/models/user';
 
 const afterCallback = async (_req, _res, session) => {
   try {
-    await createUser(session.user.email);
+    await user.create({
+      name: session.user.name,
+      email: session.user.email,
+      picture: session.user.picture,
+    });
   } catch (error) {
     console.error(error);
   }
--- pages/api/links.js
+++ pages/api/links.js
@@ -1,7 +1,7 @@
 import { getSession } from '@auth0/nextjs-auth0';
 
-import link from '@/models/link';
 import user from '@/models/user';
+import link from '@/models/link';
 
 export default async (req, res) => {
   const session = await getSession(req, res);
@@ -9,13 +9,10 @@ export default async (req, res) => {
     return res.status(401).send({ error: 'Unauthorized' });
   }
   const { user: loggedUser } = session;
-  const { url } = req.body;
-  const shortUrl = Math.random().toString(36).substr(2, 7);
-
   try {
     const dbUser = await user.get(loggedUser.email);
-    const data = await link.create(url, shortUrl, dbUser.id);
-    return res.status(200).json({ data });
+    const links = await link.getByUserId(dbUser.id);
+    return res.status(200).json({ links });
   } catch (err) {
     console.log(err);
     return res.status(500).send({ error: err.message });
