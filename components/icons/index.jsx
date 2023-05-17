01a0c2f7738539168ade7658fc8dd997a45be251
Juan Pablo Addeo <jpaddeo@gmail.com>
2023-05-16 23:35:50 -0300

    feat: :art: adding svg icons to app

---
+++ components/icons/index.jsx
@@ -0,0 +1,6 @@
+import LinkIcon from '@/components/icons/LinkIcon';
+import CopyIcon from '@/components/icons/CopyIcon';
+import CpuIcon from '@/components/icons/CpuIcon';
+import LogoutIcon from '@/components/icons/LogoutIcon';
+
+export { LinkIcon, CopyIcon, CpuIcon, LogoutIcon };
