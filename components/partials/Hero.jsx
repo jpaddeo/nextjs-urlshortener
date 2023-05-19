ec4ff60bb3189b176e372c1b2be6966476909b32
Juan Pablo Addeo <jpaddeo@gmail.com>
2023-05-18 21:22:52 -0300

    feat: :memo: adding language to hero

--- components/partials/Hero.jsx
+++ components/partials/Hero.jsx
@@ -1,27 +1,33 @@
+import { useUser } from '@auth0/nextjs-auth0';
+
 import CONFIG from '@/libs/config';
 
 import { useTranslate } from '@/hooks/useTranslate';
 
 export default function Hero() {
+  const { user, error } = useUser();
   const { lang } = useTranslate();
 
   return (
-    <section class='hero py-12'>
-      <div class='container flex flex-col items-center  justify-center mx-auto px-4'>
-        <h1 class='text-4xl font-bold text-gray-800'>
-          Blazing fast and free{' '}
-          <span className='animate-pulse font-bold italic'>forever</span> url
-          shortener
+    <section className='hero py-12'>
+      <div className='container flex flex-col items-center  justify-center mx-auto px-4'>
+        <h1 className='text-4xl font-bold text-gray-800'>
+          {lang?.hero?.description}...
+          <span className='animate-pulse font-bold italic'>
+            {lang?.hero?.description2}
+          </span>
         </h1>
-        <p class='text-lg text-gray-500 mb-4'>
-          Speed and Simplicity at Your Fingertips
+        <p className='text-lg text-gray-500 mb-4'>
+          {lang?.hero?.subdescription}
         </p>
-        <a
-          href={CONFIG.LOGIN_API_URL}
-          class='bg-blue-500 hover:bg-blue-600 text-white font-semibold py-3 px-6 rounded'
-        >
-          {lang?.index?.getStarted}
-        </a>
+        {!user && (
+          <a
+            href={CONFIG.LOGIN_API_URL}
+            className='bg-blue-500 hover:bg-blue-600 text-white font-semibold py-3 px-6 rounded'
+          >
+            {lang?.hero?.getStarted}
+          </a>
+        )}
       </div>
     </section>
   );
