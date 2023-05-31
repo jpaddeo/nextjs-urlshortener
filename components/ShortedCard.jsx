3c25457d2c7f49619d29dfe935aa47a1125c0ace
Juan Pablo Addeo <jpaddeo@gmail.com>
2023-05-30 21:59:49 -0300

    fix: :bug: fixing login legend to tooltip to show only is not logged user

--- components/ShortedCard.jsx
+++ components/ShortedCard.jsx
@@ -1,6 +1,6 @@
 import { CopyIcon, LinkIcon } from '@/components/icons';
 
-export default function ShortedCard({ origin, shorted }) {
+export default function ShortedCard({ origin, shorted, clicks }) {
   const handleCopyLink = (event, shorted) => {
     event.preventDefault();
     navigator.clipboard.writeText(shorted);
@@ -14,22 +14,29 @@ export default function ShortedCard({ origin, shorted }) {
         </span>
         <div className='flex flex-col md:flex-row gap-2 items-center justify-center'>
           <span className='font-bold text-blue-500'>{shorted}</span>
-          <button
-            onClick={(event) => handleCopyLink(event, shorted)}
-            className='border-2 border-blue-500 hover:border-dashed rounded-xl p-1 text-blue-500 font-medium'
+          <div className='flex items-center justify-center gap-2'>
+            <button
+              onClick={(event) => handleCopyLink(event, shorted)}
+              className='border-2 border-blue-500 hover:border-dashed rounded-xl p-1 text-blue-500 font-medium'
+            >
+              <span className='flex items-center justify-center gap-1'>
+                <CopyIcon />
+              </span>
+            </button>
+            <a
+              href={shorted}
+              target='_blank'
+              rel='noopener noreferrer'
+              className='border-2 border-blue-500 hover:border-dashed rounded-xl p-1 text-blue-500 font-medium'
+            >
+              <LinkIcon />
+            </a>
+          </div>
+          <span
+            className={`text-sm font-bold text-gray-800 ${clicks >= 5 ? 'bg-green-500' : clicks >= 3 ? 'bg-green-300' : 'bg-green-100'} inline-flex items-center p-1.5 rounded`}
           >
-            <span className='flex items-center justify-center gap-1'>
-              <CopyIcon />
-            </span>
-          </button>
-          <a
-            href={shorted}
-            target='_blank'
-            rel='noopener noreferrer'
-            className='border-2 border-blue-500 hover:border-dashed rounded-xl p-1 text-blue-500 font-medium'
-          >
-            <LinkIcon />
-          </a>
+            {clicks}
+          </span>
         </div>
       </div>
     </div>
